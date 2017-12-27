7d6f6dbf46271826d967ed6ac747fcec6fe780e1
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2017-12-27 15:34:47 -0800

    moire stuff

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from __future__ import division, print_function, unicode_literals, absolute_import
+
+import setuptools
+
+
+version = '2017.12.27'
+
+dependencies = ['smbus']
+
+
+setuptools.setup(install_requires=dependencies,
+                 include_package_data=True,
+                 packages=setuptools.find_packages(),
+                 version=version)
